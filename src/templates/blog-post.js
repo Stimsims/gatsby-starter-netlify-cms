15cf7230423cd558b5216832b16bfbdaa8a90ffe
illulli <illu.development@gmail.com>
2018-10-12 17:40:45 +1100

    added form 3

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -42,14 +42,13 @@ export const BlogPostTemplate = ({
           </div>
         </div>
       </div>
-      <form name="contact" method="POST" netlify>
+      <form name="contact" netlify>
         <p>
-          <label>Email: <input type="text" name="name" /></label>
+          <label>Name <input type="text" name="name" /></label>
         </p>
         <p>
-          <label>Message: <textarea name="message"></textarea></label>
+          <label>Email <input type="email" name="email" /></label>
         </p>
-        {/* <div data-netlify-recaptcha></div> */}
         <p>
           <button type="submit">Send</button>
         </p>
